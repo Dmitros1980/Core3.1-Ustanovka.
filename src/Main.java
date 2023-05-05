fcf9381d3ee1fd280458c301898e48a7e02c41b2
Dmitros <dimayurkin5@mail.ru>
2023-05-05 22:48:41 +0400

    5 commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        StringBuilder builder = new StringBuilder();
+        String[] addressFolder = {"C:\\Users\\B\\Desktop\\Games1", "C:\\Users\\B\\Desktop\\Games1\\src",
+                "C:\\Users\\B\\Desktop\\Games1\\res", "C:\\Users\\B\\Desktop\\Games1\\savegames",
+                "C:\\Users\\B\\Desktop\\Games1\\temp", "C:\\Users\\B\\Desktop\\Games1\\src\\main",
+                "C:\\Users\\B\\Desktop\\Games1\\src\\test", "C:\\Users\\B\\Desktop\\Games1\\res\\drawables",
+                "C:\\Users\\B\\Desktop\\Games1\\res\\vectors", "C:\\Users\\B\\Desktop\\Games1\\res\\icons",
+        };
+
+        String[] addressFile = {"C:\\Users\\B\\Desktop\\Games1\\src\\main\\Main.java",
+                "C:\\Users\\B\\Desktop\\Games1\\src\\main\\Utils.java",
+                "C:\\Users\\B\\Desktop\\Games1\\temp\\temp.txt."
+        };
+        for (int i = 0; i < addressFolder.length; i++) {
+            CreateFolder.createFolder(addressFolder[i]);
+        }
+        for (int j = 0; j < addressFile.length; j++) {
+            CreateFolder.createFale(addressFile[j]);
+        }
+        CreateFolder.log("C:\\Users\\B\\Desktop\\Games1\\temp\\temp.txt.");
+
+    }
+
+}
